3f18c8d983c18a3122e5c91ed38065a89a7a8c70
Thaissa <124712405+thaazs@users.noreply.github.com>
2023-11-28 18:20:44 +0000

    finalizacao indicaçao menu/fotos/textos

--- main.js
+++ main.js
@@ -7,3 +7,30 @@ function rotateCard(button) {
     const card = button.closest('.card');
     card.classList.toggle('flip');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtém o caminho da URL atual
+    var path = window.location.pathname;
+
+    // Remove a barra inicial, se houver
+    path = path.replace(/^\/|\/$/g, '');
+
+    // Encontra o link correspondente ao caminho atual
+    var activeLink = document.querySelector('.nav-link[href="' + path + '"]');
+
+    // Adiciona a classe 'active' ao link correspondente
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+});
+
+// Ativar a classe 'active' no link clicado
+var links = document.querySelectorAll('.nav-link');
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        links.forEach(function(innerLink) {
+            innerLink.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
